27af80ee3f2202dda5c7bbbf4cb082df2547a305
Hwai-En Ho <dev@hwaien.com>
2021-12-19 02:26:04 +0000

    custom font

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => (
     <NextSeo title="Everyday Psalms" />
 
     <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-      <h1 className="text-6xl font-bold">Coming soon!</h1>
+      <h1 className="text-6xl font-cinzel">Coming soon!</h1>
     </main>
   </div>
 );
