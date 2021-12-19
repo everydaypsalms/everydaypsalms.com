3e06b8d8c4788c930d472e08729e7c6b1560d6ef
Hwai-En Ho <dev@hwaien.com>
2021-12-19 05:41:21 +0000

    adding documentation

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,13 @@
 import { NextPage } from "next";
 import Layout from "../components/layout";
 
+/**
+ * A custom 404 page to replace the Next.js default static 404 page.
+ *
+ * @returns The created Next.js page.
+ *
+ * @see https://nextjs.org/docs/advanced-features/custom-error-page
+ */
 const My404: NextPage = () => (
   <Layout seoTitle="Page not found" seoDescription="Something went wrong.">
     <main className="grid place-items-center h-screen">
