59642e00be44948e981918d0ea3634dfddd0619b
Hwai-En Ho <dev@hwaien.com>
2021-12-19 05:45:55 +0000

    centralizing <head> content

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 import { NextSeo } from "next-seo";
 import { FC, PropsWithChildren } from "react";
-import Head from "next/head";
 
 /**
  * Properties for search engine optimization.
@@ -18,9 +17,6 @@ export interface LayoutProps {
 
 const Layout: FC<LayoutProps> = (p: PropsWithChildren<LayoutProps>) => (
   <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
     <NextSeo title={p.seoTitle} description={p.seoDescription} />
 
     <div className="font-cinzel">{p.children}</div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap"
             rel="stylesheet"
